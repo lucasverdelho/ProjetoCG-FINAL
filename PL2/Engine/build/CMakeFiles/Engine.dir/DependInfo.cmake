57e8c6260bc0bf11ce4a202109e7335d8e46681f
LucasVerdelho <a.lucas.s.verdelho@outlook.com>
2022-06-05 22:25:43 +0100

    make para o portatil

--- PL2/Engine/build/CMakeFiles/Engine.dir/DependInfo.cmake
+++ PL2/Engine/build/CMakeFiles/Engine.dir/DependInfo.cmake
@@ -4,18 +4,18 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Camera.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Camera.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Colour.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Colour.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Group.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Group.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Light.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Light.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Point.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Point.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Rotate.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Rotate.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Scale.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Scale.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Solid.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Solid.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Translate.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Translate.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/Triangle.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Triangle.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/main.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/main.cpp.o"
-  "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/tinyxml2/tinyxml2.cpp" "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/tinyxml2/tinyxml2.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Camera.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Camera.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Colour.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Colour.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Group.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Group.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Light.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Light.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Point.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Point.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Rotate.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Rotate.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Scale.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Scale.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Solid.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Solid.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Translate.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Translate.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/Triangle.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/Triangle.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/main.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/main.cpp.o"
+  "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/tinyxml2/tinyxml2.cpp" "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/CMakeFiles/Engine.dir/tinyxml2/tinyxml2.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- PL2/Engine/build/cmake_install.cmake
+++ PL2/Engine/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine
+# Install script for directory: /home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ubuntu/Downloads/ProjetoCG FINAL/PL2/Engine/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lv/CG PROJECT/ProjetoCG-FINAL/PL2/Engine/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
